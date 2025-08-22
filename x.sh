c98760dc0a39683c7224138d4075c1718ff21e8b
rald <thecoconutnutisagiantnut@gmail.com>
2025-08-22 15:52:22 +0000

    OK

--- x.sh
+++ x.sh
@@ -13,18 +13,12 @@ aapt package -f -m \
 	-S res \
 	-I android-30.jar
 
-JAVAFILES=""
-for JAVAFILE in $(find . -type f -name "*.java")
-do
-    JAVAFILES="$JAVAFILES $JAVAFILE"
-done
+javac -cp android-30.jar -d obj $(find src -name '*.java')
 
-ecj -cp android-30.jar -d obj $JAVAFILES
-
-dx --dex --output=output/classes.dex obj
+d8 $(find obj -name '*.class') --lib android-30.jar --output output
 
 aapt package -f -m \
-		-J gen \
+	-J gen \
     -S res \
     -M AndroidManifest.xml \
     -I android-30.jar \
@@ -36,3 +30,6 @@ zipalign -v 4 bin/game.apk bin/game-aligned.apk
 apksigner sign --ks android.keystore --ks-key-alias android --ks-pass pass:android --key-pass pass:android bin/game-aligned.apk
 
 cp bin/game-aligned.apk game.apk
+
+
+
